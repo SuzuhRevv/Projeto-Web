0196b4833394d99d6ff17adeefe5ae3b91f191ad
SuzuhRevv <joaopk04@hotmail.com>
2023-10-21 11:14:34 -0300

    Apaguei uma pasta duplicada, e troquei o nome de algumas para ficar semânticamente melhor

--- web-2-joao-victor/5-atividade/src/server.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import routes from './routes';
-
-const app = express();
-
-app.use(express.json());
-app.use('/users', routes);
-
-app.listen(7373, () => console.log('Server is running on port 7373'));
--- web-2-joao-victor/7-atividade/src/prisma/migrations/20230617053350_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `Row` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
-    `bread` INTEGER NOT NULL,
-    `value` DOUBLE NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
